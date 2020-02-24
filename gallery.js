41b75c8e0ecd5899e15b7b081e7a33c3708b00ca
deepseabunnies <emailmadisonm@gmail.com>
2020-02-23 23:06:42 -0500

    Changed games.html to index.html. Added contact info to top of page.

--- gallery.js
+++
@@ -1,8 +0,0 @@
-function expandImg(imgs, galNumber) {
-    var expandImg = document.getElementById('imgSpot' + galNumber);
-    expandImg.src = imgs.src;
-    expandImg.parentElement.style.display = "block";
-
-    var imgText = document.getElementById('textSpot' + galNumber);
-    imgText.innerHTML = imgs.alt;    
-}
